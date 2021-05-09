1b70c3f78ec45c40cf75e81af72d10f3c8e0d397
Sam Weston <weston.sam@gmail.com>
2021-05-09 10:57:29 +0100

    Run in a 10 second loop and anti-flap logic

--- shed_sched.py
+++ shed_sched.py
@@ -1,6 +1,5 @@
-# Controls the relay connected to the heater in my shed
-# If the temperature is below 20 degrees according to my BME280
-# and it's between 8 and 5 on a weekday then turn the heater on
+#!/usr/bin/env python
+# Controls the relay connected to the heater in my shed.
 
 import RPi.GPIO as GPIO
 import time
@@ -9,27 +8,33 @@ import bme280
 from datetime import datetime
 from pytz import timezone
 import argparse
+import time
 
 # Create the parser and add arguments
 parser = argparse.ArgumentParser()
 
-# Provide long form name as well (maps to 'argument3' not 'a3')
-parser.add_argument('-t', '--target-temperature', type=int, default=20)
+parser.add_argument('-t', '--target-temperature', type=float, default=20.0)
 parser.add_argument('-g', '--gpio', type=int, default=12)
 parser.add_argument('-st', '--start-time', type=int, default=8)
 parser.add_argument('-et', '--end-time', type=int, default=17)
+parser.add_argument('-ed', '--end-day', type=int, default=4)
 parser.add_argument('-tz', '--time-zone', type=str, default='Europe/London')
+parser.add_argument('-d', '--delay', type=int, default=300)
+parser.add_argument('-l', '--loop', type=int, default=10)
 
 args = parser.parse_args()
-target_temperature = args.target_temperature
-gpio_number = args.gpio
-start_time = args.start_time
-end_time = args.end_time
-time_zone = args.time_zone
+TARGET_TEMPERATURE = args.target_temperature
+RELAY_GPIO = args.gpio
+START_TIME = args.start_time
+END_TIME = args.end_time
+END_DAY = args.end_day
+TIME_ZONE = args.time_zone
+DELAY = args.delay
+LOOP = args.loop
 
-print(f'Target Temperature is {target_temperature}C')
-print(f'Using GPIO number {gpio_number}')
-print(f'Starting at {start_time}:00 and ending at {end_time}:00')
+print(f'Target Temperature is {TARGET_TEMPERATURE}°C')
+print(f'Using GPIO number {RELAY_GPIO}')
+print(f'Starting at {START_TIME}:00 and ending at {END_TIME}:00')
 
 GPIO.setwarnings(False)
 mode = GPIO.getmode()
@@ -37,9 +42,10 @@ if mode != GPIO.BCM:
     GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
     print("Setting GPIO Mode to BCM")
 
-print(f'Running setup for GPIO {gpio_number}. Setting to OUT mode.')
-RELAY_GPIO = gpio_number
+print(
+    f'Running setup for GPIO {RELAY_GPIO}. Setting to OUT mode and ensuring turned off.')
 GPIO.setup(RELAY_GPIO, GPIO.OUT)  # GPIO Assign mode
+GPIO.output(RELAY_GPIO, GPIO.LOW)  # off
 
 
 def get_temperature():
@@ -68,12 +74,12 @@ def turn_off_relay():
 
 
 def is_working_hours():
-    now = datetime.now(tz=timezone(time_zone))
+    now = datetime.now(tz=timezone(TIME_ZONE))
     hour = now.hour
     day = now.weekday()
-    if hour >= start_time and hour < end_time:
+    if hour >= START_TIME and hour < END_TIME:
         print(f'Hour {hour} is within working hours')
-        if day <= 4:  # 0 is Monday, 4 is Friday
+        if day <= END_DAY:  # 0 is Monday, 4 is Friday
             print(f'Day {day} is a working day')
             return True
         else:
@@ -84,10 +90,25 @@ def is_working_hours():
         return False
 
 
-temperature = get_temperature()
-print(f"The temperature in the shed is {temperature}")
+last_turned_off = None
+last_state = False
 
-if (temperature < target_temperature) and is_working_hours():
-    turn_on_relay()
-else:
-    turn_off_relay()
+while True:
+    temperature = get_temperature()
+    print(f'The temperature in the shed is {temperature}°C')
+
+    now = datetime.now()
+
+    if last_turned_off:
+        seconds_diff = round((now - last_turned_off).total_seconds())
+        print(f'Heater was last turned off {seconds_diff} seconds ago')
+
+    if (temperature < TARGET_TEMPERATURE) and is_working_hours():
+        if not last_state and (not last_turned_off or seconds_diff > DELAY):
+            turn_on_relay()
+            last_state = True
+    elif last_state:
+        turn_off_relay()
+        last_turned_off = now
+        last_state = False
+    time.sleep(LOOP)
