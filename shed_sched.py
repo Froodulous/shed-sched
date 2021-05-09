8d6db3e672dd4f4cd21304105a7f969d7f025a27
Sam Weston <weston.sam@gmail.com>
2021-05-09 10:01:25 +0100

    Args and bug fixes

--- shed_sched.py
+++ shed_sched.py
@@ -8,9 +8,28 @@ import smbus2
 import bme280
 from datetime import datetime
 from pytz import timezone
+import argparse
 
-target_temperature = 20
-print(f"Target Temperature is {target_temperature}C")
+# Create the parser and add arguments
+parser = argparse.ArgumentParser()
+
+# Provide long form name as well (maps to 'argument3' not 'a3')
+parser.add_argument('-t', '--target-temperature', type=int, default=20)
+parser.add_argument('-g', '--gpio', type=int, default=12)
+parser.add_argument('-st', '--start-time', type=int, default=8)
+parser.add_argument('-et', '--end-time', type=int, default=17)
+parser.add_argument('-tz', '--time-zone', type=str, default='Europe/London')
+
+args = parser.parse_args()
+target_temperature = args.target_temperature
+gpio_number = args.gpio
+start_time = args.start_time
+end_time = args.end_time
+time_zone = args.time_zone
+
+print(f'Target Temperature is {target_temperature}C')
+print(f'Using GPIO number {gpio_number}')
+print(f'Starting at {start_time}:00 and ending at {end_time}:00')
 
 GPIO.setwarnings(False)
 mode = GPIO.getmode()
@@ -18,9 +37,9 @@ if mode != GPIO.BCM:
     GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
     print("Setting GPIO Mode to BCM")
 
-print("Running setup for GPIO 12. Setting to OUT mode.")
-RELAY_1_GPIO = 12
-GPIO.setup(RELAY_1_GPIO, GPIO.OUT)  # GPIO Assign mode
+print(f'Running setup for GPIO {gpio_number}. Setting to OUT mode.')
+RELAY_GPIO = gpio_number
+GPIO.setup(RELAY_GPIO, GPIO.OUT)  # GPIO Assign mode
 
 
 def get_temperature():
@@ -40,26 +59,26 @@ def get_temperature():
 
 def turn_on_relay():
     print("Turning on relay")
-    GPIO.output(RELAY_1_GPIO, GPIO.HIGH)  # on
+    GPIO.output(RELAY_GPIO, GPIO.HIGH)  # on
 
 
 def turn_off_relay():
     print("Turning off relay")
-    GPIO.output(RELAY_1_GPIO, GPIO.LOW)  # off
+    GPIO.output(RELAY_GPIO, GPIO.LOW)  # off
 
 
 def is_working_hours():
-    now = datetime.now(tz=timezone('Europe/London'))
+    now = datetime.now(tz=timezone(time_zone))
     hour = now.hour
     day = now.weekday()
-    if hour >= 8 and hour < 17:
+    if hour >= start_time and hour < end_time:
         print(f'Hour {hour} is within working hours')
         if day <= 4:  # 0 is Monday, 4 is Friday
             print(f'Day {day} is a working day')
             return True
         else:
             print(f'Day {day} is not a working day')
-            return True
+            return False
     else:
         print(f'Hour {hour} is not within working hours')
         return False
